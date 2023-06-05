8915d6f542b2b7b029814c77b53eb1ff4db110b6
Adrian <aantosiak@icloud.com>
2023-06-05 09:13:50 +0200

    posts

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,6 +1,8 @@
 import getUser from '@/lib/getUser'
 import getUserPosts from '@/lib/getUserPosts'
-import { useId } from 'react'
+import { Suspense } from 'react'
+import UserPosts from './components/UserPosts'
+import type { Metadata } from 'next'
 
 type Params = {
   params: {
@@ -8,16 +10,33 @@ type Params = {
   }
 }
 
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId)
+  const user: User = await userData
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  }
+}
+
 export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId)
-  const userPostsData: Promise<Post[]> = getUserPosts(useId)
+  const userPostsData: Promise<Post[]> = getUserPosts(userId)
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData])
+  // const [user, userPosts] = await Promise.all([userData, userPostsData])
+  const user = await userData
+
   return (
     <>
       <h2>{user.name}</h2>
       <br />
-      <UserPosts posts={userPosts} />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        {/* @ts-expect-error Server Component */}
+        <UserPosts promise={userPostsData} />
+      </Suspense>
     </>
   )
 }
